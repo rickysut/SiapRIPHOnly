40f100f2adc3acab3801c2580c0a6b79ae6cc854
sultanjawer <bhre.astrajingga@gmail.com>
2024-05-13 00:04:10 +0700

    masih error

--- resources/views/admin/dashboard/newmap.blade.php
+++ resources/views/admin/dashboard/newmap.blade.php
@@ -8,18 +8,19 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
-	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCOT3nitE_dWhe_EMbJ3MMOtASYfI8fc4M&libraries=drawing,geometry&loading=async" ></script>
+	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCOT3nitE_dWhe_EMbJ3MMOtASYfI8fc4M&libraries=drawing,geometry,marker&loading=async" ></script>
 </head>
 <body>
 	<div id="allMap" style="height: 500px; width: 100%;" class="shadow-sm border-1"></div>
 
 	<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-	<script>
+	<script type="text/javascript">
 		function initMap() {
+			console.log('initmap')
 			map = new google.maps.Map(document.getElementById("allMap"), {
 				center: { lat: -2.548926, lng: 118.014863 },
 				zoom: 5,
-				// mapTypeId: google.maps.MapTypeId.HYBRID,
+				mapTypeId: google.maps.MapTypeId.HYBRID,
 			});
 		}
 
