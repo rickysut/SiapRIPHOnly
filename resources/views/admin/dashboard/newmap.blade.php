a1e8b371b2bcc0275748deb59ecfe30a97994a3e
sultanjawer <bhre.astrajingga@gmail.com>
2024-05-12 23:48:24 +0700

    new api

--- resources/views/admin/dashboard/newmap.blade.php
+++ resources/views/admin/dashboard/newmap.blade.php
@@ -8,7 +8,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
-	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1ea90fk4RXPswzkOJzd17W3EZx_KNB1M&libraries=drawing,geometry&loading=async" ></script>
+	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCOT3nitE_dWhe_EMbJ3MMOtASYfI8fc4M&libraries=drawing,geometry&loading=async" ></script>
 </head>
 <body>
 	<div id="allMap" style="height: 500px; width: 100%;" class="shadow-sm border-1"></div>
