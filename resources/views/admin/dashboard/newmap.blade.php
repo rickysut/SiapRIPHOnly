a8664543ff7751c85e3b5188a35acd9ce195e585
sultanjawer <bhre.astrajingga@gmail.com>
2024-05-12 23:29:20 +0700

    coba lagi

--- resources/views/admin/dashboard/newmap.blade.php
+++ resources/views/admin/dashboard/newmap.blade.php
@@ -8,12 +8,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
-	<script async defer src="https://maps.googleapis.com/maps/api/js?key={{ isset($mapkey) ? $mapkey->key : 'Default Key' }}&libraries=drawing,geometry" ></script>
+	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC1ea90fk4RXPswzkOJzd17W3EZx_KNB1M&libraries=drawing,geometry&loading=async" ></script>
 </head>
 <body>
 	<div id="allMap" style="height: 500px; width: 100%;" class="shadow-sm border-1"></div>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 	<script>
 		function initMap() {
 			map = new google.maps.Map(document.getElementById("allMap"), {
