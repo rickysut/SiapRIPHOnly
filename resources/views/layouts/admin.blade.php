5faef24e7d772657d7dde4da78af041761ad3f0e
sultanjawer <bhre.astrajingga@gmail.com>
2024-05-15 00:25:40 +0700

    test 1

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -55,7 +55,7 @@
 
 		<link href="{{ asset('css/toastr.css') }}" rel="stylesheet" />
 
-		<script async defer src="https://maps.googleapis.com/maps/api/js?key={{ isset($mapkey) ? $mapkey->key : 'Default Key' }}&libraries=drawing,geometry,marker&loading=async&v=weekly" ></script>
+		<script defer src="https://maps.googleapis.com/maps/api/js?key={{ isset($mapkey) ? $mapkey->key : 'Default Key' }}&libraries=drawing,geometry,marker&loading=async" ></script>
 
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		@yield('styles')
