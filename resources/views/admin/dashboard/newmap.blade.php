c9d55670fb5f1e659da354751aaae8784e444cd3
sultanjawer <bhre.astrajingga@gmail.com>
2024-05-15 00:07:14 +0700

    test new map

--- resources/views/admin/dashboard/newmap.blade.php
+++ resources/views/admin/dashboard/newmap.blade.php
@@ -8,12 +8,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
-	{{-- <script defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCOT3nitE_dWhe_EMbJ3MMOtASYfI8fc4M&libraries=drawing,geometry,marker&loading=async" ></script> --}}
+	<script defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCOT3nitE_dWhe_EMbJ3MMOtASYfI8fc4M&libraries=drawing,geometry,marker&loading=async" ></script>
 </head>
 <body>
 	<div id="allMap" style="height: 500px; width: 100%;" class="shadow-sm border-1"></div>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> --}}
+	<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 	<script type="text/javascript">
 		function initMap() {
 			console.log('initmap')
