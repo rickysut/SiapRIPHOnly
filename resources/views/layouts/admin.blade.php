95e8ce8312563019799e5fca6c0bab4198d24e0c
Bhre Astrajingga <158203659+BhreAstrajingga@users.noreply.github.com>
2024-05-15 08:42:05 +0700

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -55,7 +55,7 @@
 
 		<link href="{{ asset('css/toastr.css') }}" rel="stylesheet" />
 
-		<script defer src="https://maps.googleapis.com/maps/api/js?key={{ isset($mapkey) ? $mapkey->key : 'Default Key' }}&libraries=drawing,geometry,marker&loading=async" ></script>
+		<script async defer src="https://maps.googleapis.com/maps/api/js?key={{ isset($mapkey) ? $mapkey->key : 'Default Key' }}&libraries=drawing,geometry&loading=async" ></script>
 
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		@yield('styles')
