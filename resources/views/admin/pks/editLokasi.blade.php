de664fccee77bb18d85bf58b4565d9906b44ad5b
Bhre Astrajingga <bhre.astrajingga@gmail.com>
2024-07-05 15:43:34 +0700

    search lokasi edit

--- resources/views/admin/pks/editLokasi.blade.php
+++ resources/views/admin/pks/editLokasi.blade.php
@@ -248,13 +248,43 @@
 @section('scripts')
 @parent
 <script src="{{ asset('js/gmap/map.js') }}"></script>
-<script src="{{ asset('js/gmap/location-search.js') }}"></script>
+{{-- <script src="{{ asset('js/gmap/location-search.js') }}"></script> --}}
 <script src="{{ asset('js/gmap/kml_parser.js') }}"></script>
 <script src="{{ asset('js/gmap/link_parser.js') }}"></script>
 
 <script>
 	window.addEventListener('load', function() {
 		initMap();
+		function handleFormSubmit(event) {
+			event.preventDefault();
+			const input = document.getElementById("searchBox");
+			const location = input.value;
+			geocode(location);
+		}
+
+		// Define geocoding function
+		function geocode(location) {
+			const mapKey = '{{$mapkey->key}}';
+			const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${mapKey}`;
+			fetch(url)
+				.then((response) => response.json())
+				.then((data) => {
+					const { lat, lng } = data.results[0].geometry.location;
+					centerMap(lat, lng, 12);
+				})
+				.catch((error) => console.log(error));
+		}
+
+		// Define map centering function
+		function centerMap(lat, lng, zoom) {
+			const center = new google.maps.LatLng(lat, lng);
+			myMap.setCenter(center);
+			myMap.setZoom(zoom);
+		}
+
+		// Add event listener to form
+		const form = document.getElementById("location-search-form");
+		form.addEventListener("submit", handleFormSubmit);
 	});
 	// aktifkan untuk pembatasan max input luas lahan
 	// document.getElementById("luas_lahan").addEventListener("input", function() {
